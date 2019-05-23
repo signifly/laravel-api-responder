f9bb984184ea391d84dc3a816896a0a3776ebaf4
Morten Poul Jensen <mpj@signifly.com>
2019-05-23 13:19:37 +0200

    Add type hint

--- src/Concerns/Respondable.php
+++ src/Concerns/Respondable.php
@@ -3,10 +3,11 @@
 namespace Signifly\Responder\Concerns;
 
 use Signifly\Responder\Contracts\Responder;
+use Illuminate\Contracts\Support\Responsable;
 
 trait Respondable
 {
-    protected function respond($data)
+    protected function respond($data): Responsable
     {
         return app(Responder::class)->respond($data);
     }
