84763555abefc8139da095cddf56454bb99f83e4
Morten Poul Jensen <mpj@foreno.dk>
2019-05-26 21:37:32 +0200

    Allow settings statusCode on DefaultResponse

--- src/Responses/DefaultResponse.php
+++ src/Responses/DefaultResponse.php
@@ -9,13 +9,23 @@ class DefaultResponse extends Response
     /** @var mixed */
     protected $data;
 
+    /** @var int */
+    protected $statusCode = 200;
+
     public function __construct($data)
     {
         $this->data = $data;
     }
 
+    public function setStatusCode(int $statusCode): self
+    {
+        $this->statusCode = $statusCode;
+
+        return $this;
+    }
+
     public function toResponse($request)
     {
-        return new JsonResponse($this->data);
+        return new JsonResponse($this->data, $this->statusCode);
     }
 }
